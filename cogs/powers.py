afa3720b2a2a50a3c552e28b070be963c81f3edc
Nikola Rados <nikola.rados@s-sm.org>
2018-10-21 02:12:24 -0700

    Factored out power function + added @ replies

---
+++ cogs/powers.py
@@ -0,0 +1,69 @@
+import discord
+from discord.ext import commands
+
+import random
+
+
+def earned_power():
+	return random.random() < .02
+
+def random_list_item(list):
+    return list[random.randint(0, len(list)-1)]
+
+def roll_box():
+	legendary_rate = 4
+	legendary_list = ["Bass Ackwards",
+					  "Everybody Shut Up",
+					  "National Debate",
+					  "No U",
+					  "Overdrive",
+					  "Pecking Order",
+					  "Pickpocket"]
+
+	epic_rate = 14
+	epic_list = ["Age Before Beauty",
+				 "DEFCON 4",
+				 "Double Jeopardy",
+				 "Jack in the Box",
+				 "Predator Missle",
+				 "Prime Minister",
+				 "Suspension",
+				 "Tactical Nuke",
+				 "The Upsidedown"]
+
+	rare_rate = 44
+	rare_list = ["Cluster Bomb",
+				 "Hostile Takeover",
+				 "Premier",
+				 "RPG Enthusiast",
+				 "Monkey Switch",
+				 "The Senate"]
+
+	uncommon_rate = 69
+	uncommon_list = ["Ball & Chain",
+					 "Package Deal",
+					 "Saboteur",
+					 "Single Player is Dead",
+					 "Mayor",
+					 "3UP",
+					 "3DOWN"]
+
+	common_rate = 99
+	common_list = ["Bonus Round",
+				   "Over-hyped",
+				   "Quickfire",
+				   "1UP",
+				   "1DOWN"]
+
+	roll = random.randint(1, 100)
+
+	if roll <= legendary_rate:
+		return random_list_item(legendary_list)
+	elif roll <= epic_rate:
+		return random_list_item(epic_list)
+	elif roll <= rare_rate:
+		return random_list_item(rare_list)
+	elif roll <= uncommon_rate:
+		return random_list_item(uncommon_list)
+	elif roll <= common_rate:
+		return random_list_item(common_list)
---
+++ cogs/utils/constants.py
@@ -0,0 +1,10 @@
+REPLIES = [
+           ", I appreciate the mention, but I don't know what to say.",
+           ", I have a lot of work to do!",
+           ", I'm trying to craft new inventions that you can use on the index.",
+           ", you're probably my favourite but don't tell anyone, especially Murray.",
+           ", my conversation options are limited... I'm a turtle afterall.",
+           ", I just don't have time to talk!",
+           ", anymore talking and I might just go into my shell.",
+           ", funny, Sly has said the exact same thing to me before."
+          ]
